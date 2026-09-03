a64eebcb2fbaaf5a56803de67b40d7ea5a87b452
[raj parihar] <mrrajparihar281@gmail.com>
2026-09-04 01:11:39 +0530

    feat: implement security info page, result viewing UI, and comprehensive steganography logic tests.

--- lib/views/security_info_page.dart
+++ lib/views/security_info_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:data_hiding_app/theme/app_colors.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 class SecurityInfoPage extends StatelessWidget {
   const SecurityInfoPage({super.key});
